6f69755988a9b9e6db17a5aa27ea58e138d50c52
Natalie Childs <nata.faye@gmail.com>
2025-05-03 13:00:26 -0700

    user people

--- scratch.js
+++ scratch.js
@@ -1,4 +1,3 @@
-
 // Concepts for the React Class
 // 1) Functions calling other functions
 // 3) Passing parameters and returning
@@ -10,38 +9,21 @@
 // 8) try-catch is a thing and it has a format that I can look up
 // 9) validation is a thing that we do a lot as developers
 
-// Data Types
-// basic: string, boolean, number, null, undefined
-// complicated: object, array
 
 
-// How the computer simplified accessing inside objects and arrays
-// Starts fromt he left hand side and simplifies over
-// user.firstName
-// posts[2].text
-// when it sees square brackets and a number (generally speaking) it's going to index into an array
-// when it sees square brackets and a string then it's going to look for a property with that name on the object
-// when it sees a dot and then a property name it's going to look for a property on an object
 
-// In other languages you can only make objects with a class
-// In Javascript we make them without a class more often than we make them with a class
+// Objects are burritos
 
-// Making an object freehand
-// method = function that's in a property (or on an object)
-const email = {
-    id: 0, // nothing magical about it, but extremely common like a SSN for an object
-    // these are methods
-    sayHello: () => alert("hi!"),
-    sayGoodbye() {
-        alert("bye!")
-    }
-}
 
-// Javascript this magic word
-// can only be used inside of a method
-// it is really only used with OOP
-// it's rarely used in modern JS
-// it's a little buggy in JS (with arrow functions and normal functions)
+const gameBoard = [
+    [null, null, "queen", null, null],
+    [null, "king", null, null, null],
+]
+
+gameBoard[1][1]
+
+
+// COMES OUT TO AN OBJECT.propertyName
 
 
 
@@ -61,181 +43,172 @@ let tasks = [
     },
 ]
 
-// They liked this so much they made it it's own data format
-// Called JSON
-// The only difference basically is that the property names have to be in quotation marks
-// You can either make a .json file and put data in JSON format inside
-// Or you can take some data and turn it into JSON (usually a string of JSON) 
-// or take some JSON (usually in a string) and turn it into that languages data format
+let message = "Here are your tasks:\n\n"
+
+for(let i = 0; i < tasks.length; i++) {
+    // Another option: We could do this: const task = tasks[i] and then task.text on the next line
+    // message += task.text + " (" + task.tags.join(", ") + ")\n"
+
+    message += tasks[i].text + " (" + tasks[i].tags.join(", ") + ")\n"
+
+    // Another option: message += `${tasks[i].text} (${tasks[i].tags.join(", ")})\n`
+}
+
+console.log(message)
+
+// Here are your tasks:
+// * Laundry (home, annoying)
+// * Emails (work)
 
 
-tasks[1].tags[0] // "work"
+// JSON is a data storage format
+// alternatives are: Excel, SQL, CSV, Access, XML
 
-const emailTask = tasks[1] // { text: "Emails", tags: [ "work" ] }
-emailTask.tags[0] // "work"
+const jsonString = '[{"text": "Laundry","tags": ["home","annoying"]},{"text": "Emails","tags": ["work"]}]'
 
-const emailTags = tasks[1].tags // [ "work" ]
-emailTags[0] // "work"
+const csvString = "text,Laundry,tags,home,annoying"
 
-
-const emailTaskThing = tasks[1] // { text: "Emails", tags: [ "work" ] }
-const emailTagsThing = emailTaskThing.tags // [ "work" ]
-emailTagsThing[0] // "work"
-
-const myHeroes = [
-    {
-        response: "success",
-        id: "731",
-        name: "Zoom",
-        powerstats: {
-            "intelligence": "50",
-            "strength": "10",
-            "speed": "100",
-            "durability": "28",
-            "power": "100",
-            "combat": "28"
-        },
-        biography: {
-            // These quotation marks have to stay because of the dashes -
-            "full-name": "Hunter Zolomon",
-            "alter-egos": "No alter egos found.",
-            aliases: [
-                "-"
-            ],
-            "place-of-birth": "-",
-            "first-appearance": "Flash Secret Files #3",
-            publisher: "DC Comics",
-            alignment: "bad"
-        },
-        "appearance": {
-            "gender": "Male",
-            "race": "null",
-            "height": [
-                "6'1",
-                "185 cm"
-            ],
-            "weight": [
-                "181 lb",
-                "81 kg"
-            ],
-            "eye-color": "Red",
-            "hair-color": "Brown"
-        },
-        "work": {
-            "occupation": "-",
-            "base": "Keystone City, Kansas"
-        },
-        "connections": {
-            "group-affiliation": "Secret Society of Super Villains, formerly Keystone Police Department, F.B.I.",
-            "relatives": "Ashley Zolomon (ex-wife)"
-        },
-        "image": {
-            "url": "https://www.superherodb.com/pictures2/portraits/10/100/1384.jpg"
-        }
-    },
-    {
-        response: "success",
-        id: "731",
-        name: "Zoom",
-        powerstats: {
-            "intelligence": "50",
-            "strength": "10",
-            "speed": "100",
-            "durability": "28",
-            "power": "100",
-            "combat": "28"
-        },
-        biography: {
-            // These quotation marks have to stay because of the dashes -
-            "full-name": "Hunter Zolomon",
-            "alter-egos": "No alter egos found.",
-            aliases: [
-                "-"
-            ],
-            "place-of-birth": "-",
-            "first-appearance": "Flash Secret Files #3",
-            publisher: "DC Comics",
-            alignment: "bad"
-        },
-        "appearance": {
-            "gender": "Male",
-            "race": "null",
-            "height": [
-                "6'1",
-                "185 cm"
-            ],
-            "weight": [
-                "181 lb",
-                "81 kg"
-            ],
-            "eye-color": "Red",
-            "hair-color": "Brown"
-        },
-        "work": {
-            "occupation": "-",
-            "base": "Keystone City, Kansas"
-        },
-        "connections": {
-            "group-affiliation": "Secret Society of Super Villains, formerly Keystone Police Department, F.B.I.",
-            "relatives": "Ashley Zolomon (ex-wife)"
-        },
-        "image": {
-            "url": "https://www.superherodb.com/pictures2/portraits/10/100/1384.jpg"
-        }
-    }
-]
-
-// If we are trying to access a property with a dash in it, we have to use square brackets and quotation marks
-
-//heroInJS.appearance["eye-color"]
+const jsonString2 = `
+[{
+  "id": 1,
+  "first_name": "Harper",
+  "last_name": "Arnould",
+  "email": "harnould0@360.cn",
+  "gender": "Male",
+  "ip_address": "234.39.191.38"
+}, {
+  "id": 2,
+  "first_name": "Maia",
+  "last_name": "Glazyer",
+  "email": "mglazyer1@topsy.com",
+  "gender": "Female",
+  "ip_address": "196.56.123.101"
+}, {
+  "id": 3,
+  "first_name": "Raphaela",
+  "last_name": "Reding",
+  "email": "rreding2@yellowpages.com",
+  "gender": "Female",
+  "ip_address": "90.176.136.241"
+}, {
+  "id": 4,
+  "first_name": "Ricki",
+  "last_name": "McInerney",
+  "email": "rmcinerney3@mashable.com",
+  "gender": "Female",
+  "ip_address": "114.215.154.113"
+}
+`
+JSON.parse(jsonString)
 
 
-// We use try-catch when there's an error that could happen and we can't prevent that error from happening
-// #1 reason is if you're talking across the internet and you can't prevent the internet from going out
-// You CAN throw errors yourself and then catch them - rarer thing to do in Javascript
+
+// JSON and Error Handling
+JSON.parse(jsonString)
+JSON.stringify(tasks)
+
+try {
+  // code that could error and you can't prevent it
+} catch(error) {
+  // what to do if it does error
+}
+
+// this will go with fetching code in Week 5
 
 
-// blueprint to make it potentially easier to make objects
-class Message {
-    // set up any properties for us
-    constructor(author, message) { // these parameters are temporary bowls holding our ingredients to make our object soup
-        // we can use "this" to access the object we are currently constructing
-        this.id = 0
-        this.author = author
-        this.message = message
-        this.read = false
-        // this.markAsRed = function() {
-        //     this.read = true
-        // }
+
+
+// Object Oriented = structure of code where all the functions are attached to objects
+// Extremely object oriented: Java
+// Javascript is much more Function-oriented (Functional programming)
+
+
+// Class = blueprint for an object that we can use to stamp out new objects
+// method = function that's in a property
+
+class User {
+    // constructor is the code to run to construct the object's properties
+    constructor(userName, userAge) {
+        this.name = userName
+        this.age = userAge
+        this.sayHi1 = function() { console.log("Hello!") }
     }
 
-    markAsRead() {
-        this.read = true
+    sayHi2() { console.log("Hello!") }
+}
+
+// new ClassName() think constructor()
+const user1 = new User("Natalie", 35)
+const user2 = { name: "Natalie", age: 35, sayHi1: function() { console.log("Hello!") }, sayHi2: function() { console.log("Hello!") } }
+
+console.log(user1)
+console.log(user2)
+
+
+// METHODS
+
+// OOP: Why put functions on an object? For organization
+// Object oriented most common outside of Javascript
+
+class UserPerson {
+    // constructor is the code to run to construct the object's properties
+    constructor(userName, userAge) {
+        this.name = userName
+        this.age = userAge
+        this.sayHi1 = function() { console.log("Hello!") }
+    }
+
+    // This is not common in Javascript but it is outside of it!
+    setAge(newAge) { 
+        this.age = newAge 
     }
 }
 
-// If you see the magic word "new" it's calling the constructor
-// when we put a function in a property, we call it a method
-// if you see something.something() we're calling a method
-// console.log() is calling the log method on the console object
-
-const message1 = {
-    id: 0,
-    author: "Natalie",
-    message: "Hello!",
-    read: false,
-    // markAsRead() {
-    markAsRead: function() {
-        // "this" looks around at where it is and uses that object
-        this.read = true
+// Remix or a sample of another class
+class AdminUser extends UserPerson {
+    constructor(userName, userAge, adminStatus) {
+        super(userName, userAge) // call the constructor of what I extend
+        this.adminStatus = adminStatus
     }
 }
 
-const message2 = new Message("Natalie", "Hello!")
+// Modern Javascript: Why put functions on an object? To pass functions around
+// Function oriented most common inside Javascript
 
-console.log(message1, message2)
+const dataForCopyButton = {
+    text: "Copy",
+    color: "gray",
+    onClick: function() { console.log("Copied the text!") }
+}
 
-message1.markAsRead()
-message2.markAsRead()
+const dataForPasteButton = {
+    text: "Paste",
+    color: "darkgray",
+    onClick: function() { console.log("Pasted!") }
+}
 
-console.log(message1, message2)
+
+
+// Javascript Data Types
+
+// Passed by Value
+// boolean
+// string
+// number
+
+let myAge = 35
+let spouseAge = myAge // copies the value 35 into spouseAge
+myAge++
+console.log(myAge) // 36
+console.log(spouseAge) // 35
+
+// Passed by Reference
+// Why? Because they're huge
+// object
+// array
+
+let myFavoriteMusic = ["Taylor Swift", "Sabrina Carpenter"]
+let daughterFavorite = myFavoriteMusic // points this variable to the same array
+myFavoriteMusic.push("Heavy Metal")
+console.log(myFavoriteMusic) // ["Taylor Swift", "Sabrina Carpenter", "Heavy Metal"]
+console.log(daughterFavorite) // ["Taylor Swift", "Sabrina Carpenter", "Heavy Metal"]
